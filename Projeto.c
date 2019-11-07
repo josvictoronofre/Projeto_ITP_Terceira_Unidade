5b061d1074bd31c511ccd1c2549ab699ea7a2db9
Jose Victor <josvictoronofre@gmail.com>
2019-11-07 18:59:40 -0300

    ta finalmente funcionando

--- Projeto.c
+++ Projeto.c
@@ -4,10 +4,10 @@
 #include <stdlib.h>
 
 
-typedef struct cor {
-	unsigned int R = 0;
-	unsigned int B = 0;
-	unsigned int G = 0;
+typedef struct essaporranaoehumalista {
+	unsigned int R;
+	unsigned int G;
+	unsigned int B;
 	//O defalut eh a cor ser branco
 } Cor;
 
@@ -18,10 +18,29 @@ void criaImagem (Cor ** matriz) {
 
 	for (i = 0; i < 480; i++) {
 		for (j = 0; j < 640; j++) {
-			fprintf(desenho, "%hu %hu %hu", *matriz[i][j]->R, *matriz[i][j]->B, *matriz[i][j]->G);
+			fprintf(desenho, "%u %u %u", matriz[i][j].R, matriz[i][j].G, matriz[i][j].B);
 		}
 	}
+	fclose(desenho);
 
+}
+
+void escolheCor (Cor ** matriz) {
+	Cor pivo;
+	unsigned short i, j;
+	scanf("%u %u %u", &pivo.R, &pivo.G, &pivo.B);
+
+	if (pivo.R > 0 && pivo.R < 256 && pivo.G > 0 && pivo.G < 256 && pivo.B > 0 && pivo.B < 256) {
+		for (i = 0; i < 480; i ++) {
+			for (j = 0; j < 640; j ++) {
+				matriz[i][j].R = pivo.R;
+				matriz[i][j].G = pivo.G;
+				matriz[i][j].B = pivo.B;
+			}
+		}
+	} else {
+		printf("Cor invalida\n");
+	}
 }
 
 int main (void) {
@@ -34,6 +53,6 @@ int main (void) {
 	for (i = 0; i < 480; i++) {
 		matriz[i] = (Cor *) calloc(640, sizeof(Cor));
 	}
-
+	escolheCor(matriz);
 	criaImagem(matriz);
 }
