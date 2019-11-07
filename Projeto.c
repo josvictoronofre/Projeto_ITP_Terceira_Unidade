e937597482770412a2648657c5f63c65a573cad3
Jose Victor <josvictoronofre@gmail.com>
2019-11-07 18:05:00 -0300

    Eu vou reprovar.com

--- Projeto.c
+++ Projeto.c
@@ -1,16 +1,41 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
+#include <stdlib.h>
+
+void criaImagem (Cor ** matriz);
 
 typedef struct cor {
-	unsigned int R, G, B;
+	unsigned int R = 0;
+	unsigned int B = 0;
+	unsigned int G = 0;
+	//O defalut eh a cor ser branco
 } Cor;
 
 int main (void) {
-	Cor momentanea;
-	Cor *pon_moment = &momentanea;
+	Cor ** matriz;
+	unsigned short i;
+	Cor primitiva;
+	
+	//Alocando uma matriz pra imagem dinamicamente
+	matriz = (Cor **) calloc(480, sizeof(Cor *));
+	for (i = 0; i < 480; i++) {
+		matriz[i] = (Cor *) calloc(640, sizeof(Cor));
+	}
+
+	criaImagem(matriz);
 }
 
-void escolheCor (Cor *pon_moment) {
-	scanf("%d", &pon_moment->R);
+
+void criaImagem (Cor ** matriz) {
+	FILE *desenho = fopen("Imagem.ppm", "w");
+	fputs("P3\n640 480\n255\n", desenho);
+	unsigned short i, j;
+
+	for (i = 0; i < 480; i++) {
+		for (j = 0; j < 640; j++) {
+			fprintf(desenho, "%hu %hu %hu", matriz[i][j]->R, matriz[i][j]->B, matriz[i][j]->G);
+		}
+	}
+
 }
