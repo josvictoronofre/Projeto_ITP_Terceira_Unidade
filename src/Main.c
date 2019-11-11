1d690dac39317798efd6e89f6e80bf97943c3909
JOSE VICTOR ONOFRE SALES <josvictoronofre@A309-31L.imd.local>
2019-11-11 20:56:34 -0300

    Inicio de condicional pros pixels

--- src/Main.c
+++ src/Main.c
@@ -7,6 +7,11 @@ unsigned short i, j;
 
 scanf("%d %d %d", &primitiva.R, &primitiva.G, &primitiva.B); //Scanea os valores RGB pra cor primitiva
 
+if (primitiva.R > 255 || primitiva.R < 0 || primitiva.G > 255|| primitiva.G <0 || primitiva.B > 255 || primitiva.B < 0) {
+	printf("Valor invalido!");
+	return 1;
+}
+
 criaDesenho(&Resposta, primitiva);
 
 leDesenho(&Resposta);
