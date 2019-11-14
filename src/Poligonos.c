fdf7b68b66776544ca6225e39cdaa282eadcdf65
JOSE VICTOR ONOFRE SALES <josvictoronofre@A309-06L.imd.local>
2019-11-13 21:44:53 -0300

    essa reta vai me matar

--- src/Poligonos.c
+++ src/Poligonos.c
@@ -2,7 +2,7 @@
 
 
 void desenhaReta (Imagem *img, Cor primitiva) {
-	int x1, x2, y1, y2, x3, y3, i;
+	int x1, x2, y1, y2, x3, y3, i, angulo;
 
 	scanf("%d %d", &x1, &y1);
 
@@ -13,13 +13,14 @@ void desenhaReta (Imagem *img, Cor primitiva) {
 	x3 = x1;
 	y3 = y1;
 
-	if (x2 - x1 >  y2 - y1) {
-		for (i = x1; i < x2; i ++) {
-			img->MatrizDesenho[x1][y1].R = primitiva.R;
-			img->MatrizDesenho[x1][y1].G = primitiva.G;
-			img->MatrizDesenho[x1][y1].B = primitiva.B;
+	angulo = (float) (y2 - y1) / (x2 - x1);
+
+	if (y2 = y1) {
+		while (x3 != x2) {
+			img->MatrizDesenho[x3][y3].R = primitiva.R;
+			img->MatrizDesenho[x3][y3].G = primitiva.G;
+			img->MatrizDesenho[x3][y3].B = primitiva.B;
 			x3++;
-			y3 += (y2 - y1) / (x2 - x1);
 		}
 	}
 
